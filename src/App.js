9b1af34123e1c21c2b77fefa4173cb2c4786710e
grigorevpv <grigorevpv@gmail.com>
2018-06-28 15:39:04 +0300

    add combine reducers function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 
 import { Provider } from 'react-redux';
-import { combineReducers } from 'redux';
 import { createStore } from 'redux';
-
-import articlesState from './reducers/articles';
 
 import ArticleList from './ArticleList';
 import Calendar from './DayPicker';
 import {articles as articlesList} from './fixtures';
 import Filter from './Filter/Filter';
 
-const reducer = combineReducers(articlesState);
 const store = createStore(reducer);
 console.log('store = ', store)
 
